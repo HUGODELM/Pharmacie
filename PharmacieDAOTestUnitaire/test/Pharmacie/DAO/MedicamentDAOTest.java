585752f5a6d6a51046942a1d2f9bce83f4108abe
huggy <huggy@192.168.0.6>
2019-04-11 16:05:00 +0200

    test create finis, test read en cours

--- PharmacieDAOTestUnitaire/test/Pharmacie/DAO/MedicamentDAOTest.java
+++ PharmacieDAOTestUnitaire/test/Pharmacie/DAO/MedicamentDAOTest.java
@@ -6,7 +6,11 @@
 package Pharmacie.DAO;
 
 import Pharmacie.metier.Medicament;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
+import static jdk.nashorn.internal.objects.NativeString.trim;
+import myconnections.DBConnection;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,22 +23,29 @@ import static org.junit.Assert.*;
  * @author huggy
  */
 public class MedicamentDAOTest {
-    
+
+    static Connection dbConnect;
+
     public MedicamentDAOTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
+        dbConnect = DBConnection.getConnection();
+        if (dbConnect == null) {
+            System.out.println("connection invalide");
+            System.exit(1);
+        }
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -42,37 +53,61 @@ public class MedicamentDAOTest {
     /**
      * Test of read method, of class MedicamentDAO.
      */
-    @Test
+    //@Test
     public void testRead() throws Exception {
         System.out.println("read");
-        int id = 0;
+        int idmedoc = 0;
         MedicamentDAO instance = new MedicamentDAO();
-        Medicament expResult = null;
-        Medicament result = instance.read(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        instance.setConnection(dbConnect);
+        Medicament obj = new Medicament(0, "testNom", "testDesc", "testRef");
+        Medicament expResult = instance.create(obj);
+        idmedoc = expResult.getIDMEDOC();
+        Medicament result = instance.read(idmedoc);
+        assertEquals("noms différents", expResult.getNOM(), result.getNOM());
+        assertEquals("descriptions différentes", expResult.getDESCRIPTION(), result.getDESCRIPTION());
+        assertEquals("références différentes", expResult.getREFERENCE(), trim(result.getREFERENCE()));
+        assertEquals("id différents", expResult.getIDMEDOC(), result.getIDMEDOC());
+        try {
+            result = instance.read(0);
+            fail("exception d'id inconnu non générée");
+        } catch (SQLException e) {
+        }
+        instance.delete(result);
     }
 
     /**
      * Test of create method, of class MedicamentDAO.
+     *
+     * @throws java.lang.Exception
      */
+    //TODO à finir 
     @Test
     public void testCreate() throws Exception {
         System.out.println("create");
-        Medicament obj = null;
+        Medicament obj = new Medicament(0, "testNom", "testDesc", "testRef");
         MedicamentDAO instance = new MedicamentDAO();
-        Medicament expResult = null;
+        instance.setConnection(dbConnect);
+        Medicament expResult = new Medicament(0, "testNom", "testDesc", "testRef");
         Medicament result = instance.create(obj);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals("Noms différents", expResult.getNOM(), result.getNOM());
+        assertEquals("Descriptions différentes", expResult.getDESCRIPTION(), result.getDESCRIPTION());
+        assertEquals("Descriptions différentes", expResult.getREFERENCE(), trim(result.getREFERENCE()));
+        assertNotEquals("id non généré", expResult.getIDMEDOC(), result.getIDMEDOC());
+        int idmedoc = result.getIDMEDOC();
+        obj = new Medicament(0, "testNom2", "testDesc2", "testRef");
+        try {
+            Medicament result2 = instance.create(obj);
+            fail("exception de référence unique non déclenchée");
+            instance.delete(result2);
+        } catch (SQLException e) {
+        }
+        instance.delete(result);
     }
 
     /**
      * Test of update method, of class MedicamentDAO.
      */
-    @Test
+    //@Test
     public void testUpdate() throws Exception {
         System.out.println("update");
         Medicament obj = null;
@@ -87,7 +122,7 @@ public class MedicamentDAOTest {
     /**
      * Test of delete method, of class MedicamentDAO.
      */
-    @Test
+    //@Test
     public void testDelete() throws Exception {
         System.out.println("delete");
         Medicament obj = null;
@@ -100,7 +135,7 @@ public class MedicamentDAOTest {
     /**
      * Test of search method, of class MedicamentDAO.
      */
-    @Test
+    // @Test
     public void testSearch() throws Exception {
         System.out.println("search");
         String rech = "";
@@ -111,5 +146,5 @@ public class MedicamentDAOTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
 }

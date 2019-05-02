28eb45426b4aadfd1950c53b8f4f98d813cef7a6
huggy <huggy@192.168.0.6>
2019-05-02 18:44:57 +0200

    Panel de création pour les 3 classes (medecin,patient,prescription) créé et testé les boutons retour qui permettront de retourné au panel précédent ne sont cependant pas encore configuré

---
+++ PharmacieDAOTestUnitaire/src/DesignPatterns/metier/Medicament.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DesignPatterns.metier;
+
+/**
+ *
+ * @author hugo.delmarche
+ */
+public class Medicament {
+    private int idmedoc;
+    private String nom,desc,ref;
+    private Prescription;
+    //TODO continuer plus faire les autres classes 
+}
